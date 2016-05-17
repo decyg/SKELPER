649c71169cd2574b00731714ddaaca04b1d1d002
decyg <declangoldneilson@hotmail.com>
2016-05-17 16:01:26 +0100

    Added a silly basic shutdown switch for it

--- src/main/java/spring_controllers/SimpleController.java
+++ src/main/java/spring_controllers/SimpleController.java
@@ -9,16 +9,21 @@ import org.springframework.web.servlet.ModelAndView;
  * Created by Declan on 15/04/2016.
  */
 @Controller
-public class HelloWorld {
+public class SimpleController {
 
 	@RequestMapping(value="/", method = RequestMethod.GET)
-	public ModelAndView shortenUrl() {
+	public ModelAndView simpleController() {
 
 
-		ModelAndView modelAndView = new ModelAndView("shortener");
+		ModelAndView modelAndView = new ModelAndView("skelper_status");
 		modelAndView.addObject("shortenedUrl", "yeet");
 
 		return modelAndView;
+	}
+
+	@RequestMapping(value="/shutdown", method = RequestMethod.GET)
+	public void shutdownNow(){
+		System.exit(0);
 	}
 
 }
